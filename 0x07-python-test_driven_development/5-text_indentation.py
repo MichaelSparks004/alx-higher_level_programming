3a0b31f4efe6003cb35b5e47abeebac1aa85f168
MichaelSparks004 <chiazokaobasi03@gmail.com>
2022-10-20 15:56:57 -0700

    0x07-python-test_driven_development files

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
 """
-This module contains a function that indents texts
+Function that prints a text with 2 new lines after the folowwing characters
+Characters to split: ":", "?", "."
+Parameters: text must be a string otherwhise an error will raised.
 """
 
 
 def text_indentation(text):
-    '''This function prints a text with 2 new lines after each ".", "?", or ":"
-    Args:
-        text (str): The string to be printed
-    Raises:
-        TypeError: If text is not a string
-    '''
+    """Function that prints a text with 2 new lines after the folowwing
+    characters
+    Characters to split: ":", "?", "."
+    Parameters: text must be a string otherwhise an error will raised."""
 
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
+    delimiters = [".", ":", "?"]
+    i = 0
 
-    count = 0
-    while count < len(text) and text[count] == " ":
-        count = count + 1
-
-    while count < len(text):
-        print(text[count], end="")
-        if text[count] == "\n" or text[count] in ".?:":
-            if text[count] in ".?:":
+    if type(text) is str and len(text) >= 0:
+        length = len(text)
+        while i < length:
+            print(text[i], end="")
+            if text[i] in delimiters:
                 print("\n")
-            count = count + 1
-            while count < len(text) and text[count] == " ":
-                count = count + 1
-            continue
-        count = count + 1
+                try:
+                    if text[i + 1] == " ":
+                        while text[i + 1] == " ":
+                            i += 1
+                except Exception:
+                    pass
+            i += 1
+
+    else:
+        raise TypeError("text must be a string")
